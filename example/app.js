da23194e606d6e8e39b42d66035dac383cd38288
Alon <alon@empeeric.com>
2012-11-25 17:16:07 +0200

    run admin from example

--- example/app.js
+++ example/app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -13,17 +12,12 @@ app.configure(function(){
     app.set('port', process.env.PORT || 80);
     app.set('mongo', 'mongodb://localhost/formage-admin-example');
 
-    app.engine('html', require('consolidate').dust);
-    app.set('view engine', 'html');
-    app.set('views', path.join(__dirname, 'views'));
-
     app.use(express.favicon());
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(express.cookieParser('magical secret admin'));
     app.use(express.cookieSession({cookie: { maxAge: 60 * 1000 * 20 }}));
     app.use(app.router);
-    app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
@@ -33,12 +27,9 @@ app.configure('development', function(){
 
 require('mongoose').connect(app.get('mongo'));
 require('./admin')(app);
-app.get('/', function(req, res) {
-    res.redirect('/admin');
-});
 
-app.get('/dust', function(req, res){
-    res.render('index', { title: 'Dust JS' })
+app.get('/', function(req, res){
+    res.redirect('/admin');
 });
 
 http.createServer(app).listen(app.get('port'), function(){
