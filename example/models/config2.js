cc6e996689adedbb4d979996500c87fa44affdeb
Refael Ackermann <refael@empeeric.com>
2013-03-18 19:15:52 +0200

    Two new tests

---
+++ example/models/config2.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose'),
+    Types = mongoose.Schema.Types;
+
+var schema = new mongoose.Schema({
+    title: { type: String, required: true, label: 'כותרת'},
+    email: { type: String, required: true, label: 'דוא"ל'},
+    footer: {
+        social: [{
+            picture: { type: Types.Picture, label: 'תמונה'},
+            url: { type: String, label: 'קישור'}
+        }],
+        links: [{
+            text: { type: String, label: 'טקסט'},
+            url: { type: String, label: 'קישור'}
+        }]
+    },
+    mail_sent: {
+        title: { type: String, require: true, label: 'כותרת'},
+        text: { type: Types.Html, label: 'תוכן'}
+    }
+});
+
+var config = module.exports = mongoose.model('config', schema);
+config.single = true;
--- example/models/tests.js
+++ example/models/tests.js
@@ -3,16 +3,20 @@ var mongoose = require('mongoose'),
     ObjectId = Schema.Types.ObjectId;
 
 var s = new Schema({
-    list: [{
-        name: { type: String, required: true },
-        list: [{
-            name: String
-        }]
-    }],
+    list: [
+        {
+            name: { type: String, required: true },
+            list: [
+                {
+                    name: String
+                }
+            ]
+        }
+    ],
     ref: { type: ObjectId, ref: 'pages', required: true },
     string: { type: String, required: true },
     date: { type: Date, required: true },
-    enum: { type: String, enum: ['1','2','3'], required: true },
+    enum: { type: String, enum: ['1', '2', '3'], required: true },
     rich_text: { type: Schema.Types.Html, required: true },
     text: { type: Schema.Types.Text, required: true },
     image: { type: Schema.Types.File, required: true },
@@ -26,7 +30,29 @@ var s = new Schema({
                 string: { type: String, required: true }
             }
         }
-    }
+    },
+    steps: [
+        {
+            rewards: {
+                xp: {type: Schema.Types.Integer, 'default': 0},
+                cash: {min: {type: Schema.Types.Integer, min: 0, 'default': 0}, max: {type: Schema.Types.Integer, min: 0, 'default': 0}},
+                tokens: {min: {type: Schema.Types.Integer, min: 0, 'default': 0}, max: {type: Schema.Types.Integer, min: 0, 'default': 0}},
+                morale: {min: {type: Schema.Types.Integer, min: 0, 'default': 0}, max: {type: Schema.Types.Integer, min: 0, 'default': 0}},
+                reputation: {min: {type: Schema.Types.Integer, min: 0, 'default': 0}, max: {type: Schema.Types.Integer, min: 0, 'default': 0}},
+                intimidation: {min: {type: Schema.Types.Integer, min: 0, 'default': 0}, max: {type: Schema.Types.Integer, min: 0, 'default': 0}},
+                members: {min: {type: Schema.Types.Integer, min: 0, 'default': 0}, max: {type: Schema.Types.Integer, min: 0, 'default': 0}}
+            },
+            loot: {
+                items: [
+                    {item_id: {type: Schema.ObjectId, ref: 'Item', query: common.ITEM_QUERY, required: true},
+                        amount: {type: Schema.Types.Integer, min: 0, 'default': 0},
+                        percent: {type: Schema.Types.Integer, min: 0, max: 100, 'default': 0},
+                        is_mandatory: {type: Boolean, 'default': false}}
+                ]
+            },
+            action_word: {type: String}
+        }
+    ]
 });
 
 module.exports = mongoose.model('tests', s);
