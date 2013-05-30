e294e4d2adce6d269f6d0f244765ae5d933b82f0
Refael Ackermann <refael@empeeric.com>
2013-05-30 22:30:14 +0300

    Add a bugous schema

---
+++ example/models/gallery.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Types = mongoose.Schema.Types;
+
+var schema = new mongoose.Schema({
+    title: { type: String, required: true },
+    picture: Types.Picture,
+    pictures: [Types.Picture]
+});
+
+schema.methods.toString = function() {
+    return this.title;
+};
+
+var gallery = module.exports = mongoose.model('gallery', schema);
