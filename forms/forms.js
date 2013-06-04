7739aa816dcc79fd845e9e9501d5b9a6695c8caf
Refael Ackermann <refael@empeeric.com>
2013-06-04 22:37:05 +0300

    Small refactoring

--- forms/forms.js
+++ forms/forms.js
@@ -129,7 +129,8 @@ var BaseForm = exports.BaseForm = Class.extend({
         this.get_fields();
         for (var field_name in this.fields) {
             var value = this.get_value(field_name);
-            this.fields[field_name].set(value).name = field_name;
+            var field = this.fields[field_name].set(value);
+            field.name = field_name;
         }
         this._fields_ready = true;
     },
