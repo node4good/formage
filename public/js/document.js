347b466088f187880b136d038148fb2814ce2026
Refael Ackermann <refael@empeeric.com>
2013-07-10 21:52:31 +0300

    Botobox FTW!

--- public/js/document.js
+++ public/js/document.js
@@ -1,45 +1,34 @@
-var deleteDocument = function() {
-    $('#deleteButton').button('loading');
-    $.post(
-        root + '/json/dependencies',
-        {
-            model: model,
-            id: $('#document_id').val()
-        },
-        function(result){
-            var msg = result.length
-                ? 'there are some entities related to this entity: "' + result.join(', ') + '"'
-                : 'Are you sure you want to delete?';
+$(function () {
+    $('#deleteButton').click(function () {
+        $('#deleteButton').button('loading');
+        $.post(
+            root + '/json/dependencies',
+            {
+                model: window.model,
+                id: $('#document_id').val()
+            },
+            function (result) {
+                var msg = result.length
+                    ? 'there are some entities related to this entity: "' + result.join(', ') + '"'
+                    : 'Are you sure you want to delete?';
 
-            if (confirm(msg))
-                $.ajax({
-                    type: 'DELETE',
-                    url: root + '/json/model/' + model + '/document?document_id=' + encodeURIComponent($('#document_id').val()),
-                    success: function(result) {
-                        $('#deleteButton').button('reset');
-                        location.href = location.href.split('/document/')[0];
-                    },
-                    error: function(xhr, textStatus) {
-                        $('#deleteButton').button('reset');
-                        alert('Unable to delete');
-                        console.error('Deleting error', arguments);
-                    }
-                });
-            else
-                $('#deleteButton').button('reset');
-        }
-    );
-};
-
-$(function() {
-    $('#deleteButton').click(deleteDocument);
-
-    // ctrl+save to save
-    //    $(window).keypress(function(e) {
-    //        if (e.which == 115 && e.ctrlKey || event.which == 19) {
-    //            e.preventDefault();
-    //            $('form#document').submit();
-    //            return false;
-    //        }
-    //    });
+                bootbox.confirm(msg, function (res) {
+                    if (!res) return $('#deleteButton').button('reset');
+                    return $.ajax({
+                        type: 'DELETE',
+                        url: root + '/json/model/' + model + '/document?document_id=' + encodeURIComponent($('#document_id').val()),
+                        success: function () {
+                            $('#deleteButton').button('reset');
+                            location.href = location.href.split('/document/')[0];
+                        },
+                        error: function (xhr, textStatus) {
+                            $('#deleteButton').button('reset');
+                            alert('Unable to delete');
+                            console.error('Deleting error', arguments);
+                        }
+                    });
+                })
+            }
+        );
+    });
 });
