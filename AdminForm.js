453556c84e89257f1d8c405dcd90c21d0f296c25
Refael Ackermann <refael@empeeric.com>
2013-07-24 11:51:06 +0300

    Green

--- AdminForm.js
+++ AdminForm.js
@@ -7,6 +7,8 @@ var forms = require('./forms')
     , widgets = forms.widgets
     , MongooseForm = forms.forms.MongooseForm
     , jest = require('jest');
+// Lazy load crypt
+var crypt = function () {return require('./models/mongoose_admin_user').crypt;};
 
 var api_loaded = false;
 var api_path;
@@ -58,6 +60,7 @@ var AdminForm = exports.AdminForm = MongooseForm.extend({
 });
 
 
+//noinspection JSUnusedGlobalSymbols
 var _JestAdminResource = jest.Resource.extend({
     init: function () {
         this._super();
@@ -67,28 +70,30 @@ var _JestAdminResource = jest.Resource.extend({
             text: null
         };
 
+        //noinspection JSUnusedGlobalSymbols
         this.allowed_methods = ['get'];
 
+        //noinspection JSUnusedGlobalSymbols
         this.filtering = {
             data: null,
             query: null,
-            id:null
+            id: null
         };
     },
 
     get_objects: function (req, filters, sorts, limit, offset, callback) {
         var data = JSON.parse(filters.data);
         var model = mongoose.model(data.model);
-        if(filters.id){
-            model.findById(filters.id,function(err,doc){
-                var result = doc && {id:doc.id,text:doc.toString()};
-                callback(err,result);
+        if (filters.id) {
+            model.findById(filters.id, function (err, doc) {
+                var result = doc && {id: doc.id, text: doc.toString()};
+                callback(err, result);
             });
         }
         else {
             var escaped_filters = filters.query.replace(_escaper, "\\$&");
             var query = data.query.replace(/__value__/g, escaped_filters);
-            model.find({$where: query}).limit(40).exec(function (err, results) {
+            model.find({'$where': query}).limit(limit).skip(offset).sort(sorts).exec(function (err, results) {
                 if (results) {
                     if (results.objects) {
                         results = results.objects;
@@ -110,66 +115,52 @@ exports.loadApi = function (app, path) {
 };
 
 
-var crypt = require('./models/mongoose_admin_user').crypt;
-
-
 exports.AdminUserForm = AdminForm.extend({
-    init:function(request,options)
-    {
-        this._super(request,options,mongoose.model('_MongooseAdminUser'));
-    }
-    ,get_fields:function(){
+    init: function (request, options) {
+        this._super(request, options, mongoose.model('_MongooseAdminUser'));
+    },
+
+
+    get_fields: function () {
         this._super();
         var fields = this.fields;
-
         delete fields['passwordHash'];
-
-
-        this.fields['current_password'] = new forms.fields.StringField({widget:forms.widgets.PasswordWidget,label:'Current Password'});
-
-        this.fields['password'] = new forms.fields.StringField({widget:forms.widgets.PasswordWidget,label:'New Password'});
-
-        this.fields['password_again'] = new forms.fields.StringField({widget:forms.widgets.PasswordWidget,label:'Again'});
-
-        this.fieldsets[0].fields = ['username','is_superuser','permissions','current_password','password','password_again'];
-
+        this.fields['current_password'] = new forms.fields.StringField({widget: forms.widgets.PasswordWidget, label: 'Current Password'});
+        this.fields['password'] = new forms.fields.StringField({widget: forms.widgets.PasswordWidget, label: 'New Password'});
+        this.fields['password_again'] = new forms.fields.StringField({widget: forms.widgets.PasswordWidget, label: 'Again'});
+        //noinspection JSUnresolvedVariable
+        this.fieldsets[0].fields = ['username', 'is_superuser', 'permissions', 'current_password', 'password', 'password_again'];
         return fields;
     },
 
-    is_valid:function(callback)
-    {
-        var self = this;
-        this._super(function(err,result)
-        {
-            if(err || !result)
-                callback(err,result);
-            else
-            {
-                if(self.data.password) {
-                    if(!crypt.compareSync(self.data.current_password,self.instance.passwordHash))
-                        self.errors['current_password'] = self.fields['current_password'].errors = ['Password incorrect'];
-                    else
-                    {
-                        if(self.data.password != self.data.password_again)
-                        {
-                            self.errors['password_again'] = self.fields['password_again'].errors = ['typed incorrectly'];
-                        }
-                    }
 
+    is_valid: function (callback) {
+        var self = this;
+        this._super(function (err, result) {
+            if (err || !result) return callback(err, result);
+            var data = self.data || {current_password: '', password_again: 'x'};
+            if (data.password) {
+                if (crypt().compareSync(data.current_password, self.instance.passwordHash)) {
+                    if (data.password != data.password_again) {
+                        self.errors['password_again'] = self.fields['password_again'].errors = ['typed incorrectly'];
+                    }
+                } else {
+                    self.errors['current_password'] = self.fields['current_password'].errors = ['Password incorrect'];
                 }
-                else{
-                    delete self.data.password;
-                    delete self.data.current_password;
-                    delete self.data.password;
-                }
-                callback(null,Object.keys(self.errors).length == 0);
+            } else {
+                delete data.password;
+                delete data.current_password;
+                delete data.password;
             }
+            var hasErrors = Object.keys(self.errors).length;
+            return callback(null, !hasErrors);
         });
     },
-    actual_save:function(callback)
-    {
-        if(this.data.password)
-            this.instance.passwordHash = crypt.encryptSync(this.data.password);
+
+
+    actual_save: function (callback) {
+        if (this.data.password)
+            this.instance.passwordHash = crypt().encryptSync(this.data.password);
         this._super(callback);
     }
 });
