0cb957fb2fa64a4bb701fa04e3b4bb64470025de
Refael Ackermann <refael@empeeric.com>
2013-08-20 15:25:03 +0300

    Custom form code example

--- example/models/config.js
+++ example/models/config.js
@@ -1,19 +1,24 @@
 var mongoose = require('mongoose'),
-    Types = mongoose.Schema.Types;
+    Types = mongoose.Schema.Types,
+    AdminForm = require('../../AdminForm').AdminForm;
+
 
 var schema = new mongoose.Schema({
     title: { type: String, required: true, label: 'כותרת'},
     email: { type: String, required: true, label: 'דוא"ל'},
-
     footer: {
-        social: [{
-            picture: { type: Types.Picture, label: 'תמונה'},
-            url: { type: String, label: 'קישור'}
-        }],
-        links: [{
-            text: { type: String, label: 'טקסט'},
-            url: { type: String, label: 'קישור'}
-        }]
+        social: [
+            {
+                picture: { type: Types.Picture, label: 'תמונה'},
+                url: { type: String, label: 'קישור'}
+            }
+        ],
+        links: [
+            {
+                text: { type: String, label: 'טקסט'},
+                url: { type: String, label: 'קישור'}
+            }
+        ]
     },
     mail_sent: {
         title: { type: String, require: true, label: 'כותרת'},
@@ -21,6 +26,18 @@ var schema = new mongoose.Schema({
     }
 });
 
-var config = module.exports = mongoose.model('config', schema);
-config.single = true;
-config.label = 'הגדרות';
+
+var model = module.exports = mongoose.model('config', schema);
+model.single = true;
+model.label = 'הגדרות';
+model.formage = {
+    form: AdminForm.extend({
+        render: function (res, options) {
+            this._super(res, options);
+            res.write(
+                "<div class='watermark'>Custom Form Code</div>" +
+                "<style>.watermark {position: absolute; top: 50%; opacity: 0.25; font-size: 3em; width: 50%; text-align: center; z-index: 1000; transform:rotate(-45deg); -ms-transform: rotate(-45deg); -webkit-transform: rotate(-45deg); }</style>"
+            );
+        }
+    })
+};
