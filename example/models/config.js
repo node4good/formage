f7211aea252759c97ab440150240132d0ab012cb
Refael Ackermann <refael@empeeric.com>
2013-09-25 18:02:48 +0300

    Flattened and remove 'sji' from Forms

--- example/models/config.js
+++ example/models/config.js
@@ -1,7 +1,6 @@
 //noinspection JSUnresolvedVariable
 var mongoose = require('mongoose'),
-    Types = mongoose.Schema.Types,
-    AdminForm = require('../..').AdminForm;
+    Types = mongoose.Schema.Types;
 
 var schema = new mongoose.Schema({
     title: { type: String, required: true, label: 'כותרת'},
@@ -26,19 +25,9 @@ var schema = new mongoose.Schema({
     }
 });
 
-
 var model = module.exports = mongoose.model('config', schema);
 model.single = true;
 model.label = 'הגדרות';
 model.formage = {
-    section: 'Configuration',
-    form: AdminForm.extend({
-        render: function (res, options) {
-            this._super(res, options);
-            res.write(
-                "<div class='watermark'>Custom Form Code</div>" +
-                "<style>.watermark {position: absolute; top: 50%; opacity: 0.25; font-size: 3em; width: 50%; text-align: center; z-index: 1000; transform:rotate(-45deg); -ms-transform: rotate(-45deg); -webkit-transform: rotate(-45deg); }</style>"
-            );
-        }
-    })
+    section: 'Configuration'
 };
--- example/models/extend.js
+++ example/models/extend.js
@@ -1,12 +1,14 @@
 //noinspection JSUnresolvedVariable
 var mongoose = require('mongoose'),
+    MongooseSchema = mongoose.Schema,
+    MongooseTypes = mongoose.Types,
     util = require('util'),
     formage = require('../../');
 
 
 var ReversedWidget = formage.widgets.TextWidget.extend({
     render: function (res) {
-        this.value = this.value.split("").reverse().join("");
+        this.value = (this.value || '').split("").reverse().join("");
         this.attrs.style = '-moz-transform: scale(-1, 1); -webkit-transform: scale(-1, 1); transform: scale(-1, 1);';
         this._super(res);
     }
@@ -64,26 +66,29 @@ var TwoDField = formage.fields.StringField.extend({
 var TwoD = function TwoD(path, options) {
     TwoD.super_.call(this, path, options);
 };
-util.inherits(TwoD, mongoose.Schema.Types.Mixed);
-mongoose.Types.TwoD = Object;
-mongoose.Schema.Types.TwoD = TwoD;
+util.inherits(TwoD, MongooseSchema.Types.Mixed);
+MongooseTypes.TwoD = Object;
+MongooseSchema.Types.TwoD = TwoD;
 
 
-var schema = new mongoose.Schema({
+var schema = new MongooseSchema({
     reversed: { type: String, formageField: ReversedField},
     two_d: { type: TwoD, formageField: TwoDField}
 });
-
-
 var model = module.exports = mongoose.model('extend', schema);
+
+var CustomForm = function () {
+    this.init.apply(this, arguments);
+};
+util.inherits(CustomForm, formage.AdminForm);
+CustomForm.prototype.render = function (res, options) {
+    formage.AdminForm.prototype.render.call(this, res, options);
+    res.write(
+        "<div class='watermark'>Extending with Custom Form</div>" +
+            "<style>.watermark {position: absolute; top: 50%; opacity: 0.25; font-size: 3em; width: 50%; text-align: center; z-index: 1000; transform:rotate(45deg); -ms-transform: rotate(45deg); -webkit-transform: rotate(45deg); }</style>"
+    );
+};
+
 model.formage = {
-    form: formage.AdminForm.extend({
-        render: function (res, options) {
-            this._super(res, options);
-            res.write(
-                "<div class='watermark'>Extending with Custom Form</div>" +
-                "<style>.watermark {position: absolute; top: 50%; opacity: 0.25; font-size: 3em; width: 50%; text-align: center; z-index: 1000; transform:rotate(45deg); -ms-transform: rotate(45deg); -webkit-transform: rotate(45deg); }</style>"
-            );
-        }
-    })
+    form: CustomForm
 };
