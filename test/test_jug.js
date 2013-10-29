d9b31709be9c3e26f0c9710705fe9d34e29ffe35
Refael Ackermann <refael@empeeric.com>
2013-10-29 17:45:37 +0200

    Able to test juggling+mssql

---
+++ test/test_jug.js
@@ -0,0 +1,58 @@
+'use strict';
+var nodestrum = require("nodestrum");
+nodestrum.register_process_catcher();
+
+Error.stackTraceLimit = Infinity;
+var express = require('express');
+var sinon = require('sinon');
+
+var Schema = require("jugglingdb").Schema;
+var schema = new Schema("mssql", {host: "(LocalDB)\\v11.0", database: "maskar"});
+
+
+module.exports = {
+    "Mock test first page": function (test) {
+        schema.on("connected", function () {
+
+                var AppliesTo = schema.define("AppliesTo", {
+                    AppliesToID: {type: Number, primaryKey: true},
+                    Title: {type: String, limit: 100},
+                    Identifier: {type: String, limit: 100},
+                    Editable: {type: Number}
+                });
+
+//                schema.automigrate(function () {
+//
+//                    var at = new AppliesTo;
+//                    at.Title = "gaga";
+//                    at.save(function (err, doc) {
+//                        console.log(doc);
+//                    });
+//                });
+                var app = express();
+                var formage = require('../lib/index');
+                //noinspection JSUnresolvedVariable
+                var admin_app = app.admin_app;
+                var admin = formage.init(app, express, {AppliesTo: AppliesTo}, {
+                    title: 'Formage Example',
+                    default_section: 'Main',
+                    admin_users_gui: true
+                });
+                var mock_req = {
+                    params: {modelName: "AppliesTo", documentId: "1"},
+                    session: {_mongooseAdminUser: {}},
+                    query: {},
+                    method: "GET",
+                    app: admin_app
+                };
+                var mock_res = {};
+                var cb = sinon.spy();
+                admin_app.routes.get[4].callbacks[1](mock_req, mock_res, cb);
+                sinon.assert.calledOnce(cb);
+                test.done();
+            }
+        );
+    }
+};
+
+
---
+++ utils/testJugMs.js
@@ -0,0 +1,61 @@
+"use strict";
+var nodestrum = require("nodestrum");
+nodestrum.register_process_catcher();
+
+var Schema = require("jugglingdb").Schema;
+var schema = new Schema("mssql", {host: "(LocalDB)\\v11.0", database: "maskar"});
+
+schema.on("connected", function () {
+
+        var AppliesTo = schema.define("AppliesTo", {
+            AppliesToID: {
+                type: Number,
+                primaryKey: true
+            },
+            Title: {
+                type: String,
+                limit: 100
+            },
+            Identifier: {
+                type: String,
+                limit: 100
+            },
+            Editable: {
+                type: Number
+            }
+        });
+
+        schema.automigrate(function () {
+
+            var at = new AppliesTo;
+            at.Title = "gaga";
+            at.save(function (err, doc) {
+                console.log(doc);
+            });
+        });
+
+        var express = require('express'),
+            http = require('http'),
+            path = require('path'),
+            app = express();
+
+        try {
+            var formage = require('../lib/index');
+            var admin = formage.init(app, express, {AppliesTo: AppliesTo}, {
+                title: 'Formage Example',
+                default_section: 'Main',
+                admin_users_gui: true
+            });
+        } catch (e) {
+            console.log(e.stack);
+        }
+
+        http.createServer(app).listen(80, function () {
+            console.log('Express server listening on port ' + 80);
+        });
+
+        exports.app = app;
+
+    }
+)
+;
