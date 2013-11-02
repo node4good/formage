e7351c3c784af5379a0f999ceee0ab293934a6f4
Refael Ackermann <refael@empeeric.com>
2013-11-02 17:30:31 +0200

    more & less

--- utils/tests-common.js
+++ utils/tests-common.js
@@ -5,7 +5,7 @@ global.chai = require('chai');
 global.should = chai.should();
 chai.Assertion.includeStack = true;
 process.env.FORMAGE_DISABLE_DOMAINS = true;
-process.env.MONGOOSE_DISABLE_STABILITY_WARNING;
+process.env.MONGOOSE_DISABLE_STABILITY_WARNING = true;
 
 global.mock_req_proto = {
     params: {},
@@ -16,21 +16,5 @@ global.mock_req_proto = {
 
 global.mock_res_proto = {
     setHeader: function () {},
-    status: function (val) {this._status = val;},
-    send: function (status, val) {
-        if (status === 500)
-            console.log(val.stack || val);
-    },
-    render: function (view, options) {
-        options = options || {};
-        var self = this
-            , req = this.req
-            , app = req.app;
-
-        // merge res.locals
-        options._locals = self.locals;
-
-        // render
-        app.render(view, options, this.end);
-    }
+    status: function (val) {this._status = val;}
 };
