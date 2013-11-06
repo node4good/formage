816e78f468587f94c0c13a23890436e9618c84fd
Refael Ackermann <refael@empeeric.com>
2013-11-06 20:55:50 +0200

    Test for 'singe' models

--- test/test-mongoose.js
+++ test/test-mongoose.js
@@ -1,5 +1,6 @@
 'use strict';
 describe("high level REST requests on mongoose", function () {
+    this.timeout(2000);
     var ctx = {};
     before(function (done) {
         _.each(require.cache, function (mod, modName) {
@@ -19,9 +20,10 @@ describe("high level REST requests on mongoose", function () {
                 }));
                 var tests = require('../example/classic/models/tests');
                 var pages = require('../example/classic/models/pages');
+                var config = require('../example/classic/models/config');
                 var express = require('express');
                 var app = express();
-                formage.init(app, express, {pages: pages, AppliesTo: AppliesTo, Tests: tests}, {
+                formage.init(app, express, {pages: pages, AppliesTo: AppliesTo, Tests: tests, config: config}, {
                     title: 'Formage Example',
                     default_section: 'Main',
                     admin_users_gui: true
@@ -29,6 +31,29 @@ describe("high level REST requests on mongoose", function () {
                 ctx.app = mock_req_proto.app = app.admin_app;
                 done(err);
             })
+        });
+    });
+
+
+    describe('just bugs', function () {
+        it('test that a single model works', function (done) {
+            var mock_req = _.defaults({
+                url: "/model/config/document/single",
+                method: "GET"
+            }, mock_req_proto);
+
+            var mock_res = _.defaults({ req: mock_req }, mock_res_proto);
+
+            mock_res.render = function (view, options) {
+                view.should.equal("document.jade");
+                should.exist(options);
+                this.req.app.render(view, options, function (err, doc) {
+                    should.exist(doc);
+                    done(err);
+                });
+            };
+
+            ctx.app.handle(mock_req, mock_res);
         });
     });
 
