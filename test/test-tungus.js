9120181ef891a9152dd5aeb8b18519cdeb63d23c
Refael Ackermann <refael@empeeric.com>
2013-11-06 17:04:41 +0200

    Refactoring register model

--- test/test-tungus.js
+++ test/test-tungus.js
@@ -11,23 +11,25 @@ describe("high level REST requests on tungus", function () {
         var mongoose = ctx.mongoose = require("mongoose");
         var conn_str = 'tingodb://./.data/' + this.test.parent.title.replace(/\s/g, '');
         mongoose.connect(conn_str, function (err) {
-            if (err) done(err);
-            var AppliesTo = mongoose.model('AppliesTo', new mongoose.Schema({
-                Title: {type: String, limit: 100, required: true},
-                Identifier: {type: String, limit: 100},
-                Editable: {type: Number}
-            }));
-            var tests = require('../example/classic/models/tests');
-            var pages = require('../example/classic/models/pages');
-            var express = require('express');
-            var app = express();
-            formage.init(app, express, {pages: pages, AppliesTo: AppliesTo, Tests: tests}, {
-                title: 'Formage Example',
-                default_section: 'Main',
-                admin_users_gui: true
+            if (err) return done(err);
+            return mongoose.connection.db.dropDatabase(function (err, doc) {
+                var AppliesTo = mongoose.model('AppliesTo', new mongoose.Schema({
+                    Title: {type: String, limit: 100, required: true},
+                    Identifier: {type: String, limit: 100},
+                    Editable: {type: Number}
+                }));
+                var tests = require('../example/classic/models/tests');
+                var pages = require('../example/classic/models/pages');
+                var express = require('express');
+                var app = express();
+                formage.init(app, express, {pages: pages, AppliesTo: AppliesTo, Tests: tests}, {
+                    title: 'Formage Example',
+                    default_section: 'Main',
+                    admin_users_gui: true
+                });
+                ctx.app = mock_req_proto.app = app.admin_app;
+                done(err);
             });
-            ctx.app = mock_req_proto.app = app.admin_app;
-            done()
         });
     });
 
