14e24be856f3e5c4e411a0899b70d7634ac8658a
Refael Ackermann <refael@empeeric.com>
2013-11-06 15:11:27 +0200

    Refactoring names

--- test/test-tungus.js
+++ test/test-tungus.js
@@ -9,7 +9,7 @@ describe("high level REST requests on tungus", function () {
         require('tungus');
         var formage = require('../index');
         var mongoose = ctx.mongoose = require("mongoose");
-        var conn_str = 'tingodb://./data/' + this.test.parent.title.replace(/\s/g, '');
+        var conn_str = 'tingodb://./.data/' + this.test.parent.title.replace(/\s/g, '');
         mongoose.connect(conn_str, function (err) {
             if (err) done(err);
             var AppliesTo = mongoose.model('AppliesTo', new mongoose.Schema({
