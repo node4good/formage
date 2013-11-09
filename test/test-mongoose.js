0d1a61c012eea1db401217aeedad152c3e19b20d
Refael Ackermann <refael@empeeric.com>
2013-11-09 14:22:15 +0200

    Test for #41

--- test/test-mongoose.js
+++ test/test-mongoose.js
@@ -33,48 +33,94 @@ describe("high level REST requests on mongoose", function () {
         });
     });
 
+    function step1(done) {
+        var mock_req = _.defaults({
+            url: "/model/config/document/single",
+            method: "POST",
+            body: {
+                title: 'ref1',
+                'footer.links_li0_text': 'tgf2',
+                'footer.links_li0_url': 'yhg2'
+            }
+        }, mock_req_proto);
+        var mock_res = _.defaults({ req: mock_req }, mock_res_proto);
+        mock_res.render = function (view, options) {
+            done(options.form.errors);
+        };
+        mock_res.redirect = function (url) {
+            url.should.equal("/admin/model/config");
+            step2(done);
+        };
+        ctx.app.handle(mock_req, mock_res);
+    }
+
+    function step2(done) {
+        var mock_req = _.defaults({
+            url: "/model/config/document/single",
+            method: "GET"
+        }, mock_req_proto);
+
+        var mock_res = _.defaults({ req: mock_req }, mock_res_proto);
+
+        mock_res.render = function (view, options) {
+            view.should.equal("document.jade");
+            should.exist(options);
+            options.form.fields["footer.links"].fields.text.value.should.eqaul("tgf2");
+            options.form.fields["footer.links"].fields.url.value.should.equal("yhg2");
+            this.req.app.render(view, options, function (err, doc) {
+                if (err) throw err;
+                should.exist(doc);
+                Boolean(~doc.indexOf(' value="tgf2" class="optional" type="text" name="footer.links_li0_text"'))
+                    .should.equal(true);
+                step3(done);
+            });
+        };
+        ctx.app.handle(mock_req, mock_res);
+    }
+
+    function step3(done) {
+        var mock_req = _.defaults({
+            url: "/model/config/document/single",
+            method: "POST",
+            body: {'footer.links_li0_text': ''}
+        }, mock_req_proto);
+        var mock_res = _.defaults({ req: mock_req }, mock_res_proto);
+        mock_res.render = function (view, options) {
+            done(options.form.errors);
+        };
+        mock_res.redirect = function (url) {
+            url.should.equal("/admin/model/config");
+            step4(done);
+        };
+
+        ctx.app.handle(mock_req, mock_res);
+    }
+
+    function step4(done) {
+        var mock_req = _.defaults({
+            url: "/model/config/document/single",
+            method: "GET"
+        }, mock_req_proto);
+
+        var mock_res = _.defaults({ req: mock_req }, mock_res_proto);
+
+        mock_res.render = function (view, options) {
+            view.should.equal("document.jade");
+            should.exist(options);
+            this.req.app.render(view, options, function (err, doc) {
+                if (err) throw err;
+                should.exist(doc);
+                Boolean(~doc.indexOf(' value="" class="optional" type="text" name="footer.links_li0_text"'))
+                    .should.equal(true);
+                return done();
+            });
+        };
+        ctx.app.handle(mock_req, mock_res);
+    }
+
     describe("nested & embeded", function () {
-        it("should get updated", function (done) {
-            var mock_req = _.defaults({
-                url: "/model/config/document/single",
-                method: "POST",
-                body: {
-                    title: 'ref1',
-                    email: 'ref1',
-                    'footer.links_li0_text': 'tgf2',
-                    'footer.links_li0_url': 'yhg2',
-                    'mail_sent.title': '',
-                    'mail_sent.text': ''
-                }
-            }, mock_req_proto);
-            var mock_res = _.defaults({ req: mock_req }, mock_res_proto);
-            mock_res.render = function (view, options) {
-                done(options.form.errors);
-            };
-            mock_res.redirect = function (url) {
-                url.should.equal("/admin/model/config");
-                var mock_req = _.defaults({
-                    url: "/model/config/document/single",
-                    method: "GET"
-                }, mock_req_proto);
-
-                var mock_res = _.defaults({ req: mock_req }, mock_res_proto);
-
-                mock_res.render = function (view, options) {
-                    view.should.equal("document.jade");
-                    should.exist(options);
-                    this.req.app.render(view, options, function (err, doc) {
-                        if (err) return done(err);
-                        should.exist(doc);
-                        Boolean(~doc.indexOf(' value="tgf2" class="optional" type="text" name="footer.links_li0_text"'))
-                            .should.equal(true);
-                        return done();
-                    });
-                };
-                ctx.app.handle(mock_req, mock_res);
-            };
-
-            ctx.app.handle(mock_req, mock_res);
+        it.skip("should get updated", function (done) {
+            step1(done);
         });
     });
 
@@ -87,6 +133,7 @@ describe("high level REST requests on mongoose", function () {
         delete ctx.mongoose;
         delete ctx.app;
     });
-});
+})
+;
 
 var section_snippet = '<div class="section"><h2><span>Configuration</span></h2><ul class="models"><li><div class="btn-group pull-right"><a href="/admin/model/config/document/single" class="btn btn-default">Edit</a></div><a href="/admin/model/config/document/single"><h3>הגדרות</h3></a></li></ul></div>';
