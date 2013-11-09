2fdac3f4a8e17afe9fb8582be221653dba6a7c9c
Refael Ackermann <refael@empeeric.com>
2013-11-09 15:09:28 +0200

    Test don't stall

--- test/common/tests-common.js
+++ test/common/tests-common.js
@@ -21,8 +21,18 @@ global.mock_res_proto = {
     setHeader: function () {},
     status: function (val) {this._status = val;},
     output: {push: _.identity},
-    outputEncodings: {push: _.identity}
+    outputEncodings: {push: _.identity},
+    render: function () {throw new Error(arguments);},
+    redirect: function () {throw new Error(arguments);}
 };
+
+
+global.makeRes = function makeRes(req, done) {
+    var res = _.defaults({ req: req }, mock_res_proto);
+    res.send = function (status, err) {done(err);};
+    return res;
+};
+
 
 var fs = require('fs');
 global.test_post_body_multipart = fs.readFileSync('test/fixtures/test-post-body.mime', 'utf-8');
