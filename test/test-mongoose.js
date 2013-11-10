69717a6f938bda149dfc8f70ddf8923f62d26289
Refael Ackermann <refael@empeeric.com>
2013-11-10 16:40:10 +0200

    Testing improvments

--- test/test-mongoose.js
+++ test/test-mongoose.js
@@ -1,4 +1,5 @@
 'use strict';
+/*global makeRes */
 describe("high level REST requests on mongoose", function () {
     var ctx = {};
     before(function (done) {
@@ -118,6 +119,60 @@ describe("high level REST requests on mongoose", function () {
         ctx.app.handle(mock_req, mock_res);
     }
 
+    it.only("post", function (done) {
+        var mock_req = _.defaults({
+            url: "/model/Tests/document/new",
+            method: "POST",
+            headers: {},
+            body: {
+                string_req: "gaga",
+                num_with_params: "0",
+                enum: "",
+                'list_o_numbers_li0___self__': "0",
+                'list_o_numbers_li1___self__': "1",
+                'list_o_numbers_li2___self__': "2",
+                'list_li0_name': 'ggg',
+                'list_li0_list_li0_name': 'hhh',
+                'list_li0_list_li1_name': 'jjj',
+                'object.object.object.nested_string_req': "gigi"
+            }
+        }, mock_req_proto);
+        var mock_res = makeRes(mock_req, done);
+
+        mock_res.redirect = function (url) {
+            var doc = this._debug_document;
+            Number(0).should.equal(url.indexOf("/admin/model/Test"));
+            var mock_req = _.defaults({
+                url: '/model/Tests/document/' + doc.id,
+                method: "GET"
+            }, mock_req_proto);
+
+            var mock_res = makeRes(mock_req, done);
+
+            mock_res.render = function (view, options) {
+                view.should.equal("document.jade");
+                should.exist(options);
+
+                var instance = options.form.instance;
+                instance.string_req.should.equal("gaga");
+                Number(instance.num_with_params).should.equal(0);
+                should.not.exist(instance.enum);
+                instance.object.object.object.nested_string_req.should.equal("gigi");
+                Number(instance.list_o_numbers[0]).should.equal(0);
+
+                this.req.app.render(view, options, function (err, doc) {
+                    if (err) return done(err);
+                    should.exist(doc);
+                    doc.replace(/\r|\n/g, '').replace(/pages\?ref=.+"/, '').should.equal(renderedDoc);
+                    return done();
+                });
+            };
+            ctx.app.handle(mock_req, mock_res);
+        };
+        ctx.app.handle(mock_req, mock_res);
+    });
+
+
     describe("nested & embeded", function () {
         it.skip("should get updated", function (done) {
             step1(done);
@@ -137,3 +192,4 @@ describe("high level REST requests on mongoose", function () {
 ;
 
 var section_snippet = '<div class="section"><h2><span>Configuration</span></h2><ul class="models"><li><div class="btn-group pull-right"><a href="/admin/model/config/document/single" class="btn btn-default">Edit</a></div><a href="/admin/model/config/document/single"><h3>הגדרות</h3></a></li></ul></div>';
+var renderedDoc = require('fs').readFileSync('test/fixtures/rendered_doc.text', 'utf-8').replace(/\r|\n/g, '').replace(/pages\?ref=.+"/, '');
