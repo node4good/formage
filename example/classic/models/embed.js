f3bd55ce03ff04584eff629f977a133854bebac0
Refael Ackermann <refael@empeeric.com>
2013-11-25 11:45:28 +0200

    Testing without domains

--- example/classic/models/embed.js
+++ example/classic/models/embed.js
@@ -18,7 +18,8 @@ var schema = new mongoose.Schema({
                         nested_string_req5: { type: String, required: true },
                         list5:[Number],
                         pic: SchemaTypes.Picture,
-                        pics: [SchemaTypes.Picture]
+                        pics: [SchemaTypes.Picture],
+                        refs: [{ type: SchemaTypes.ObjectId, ref: 'pages' }]
                     }
                 }]
             }
