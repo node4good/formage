8f173a01a349fb4e090ad3f96920835571186ba0
Refael Ackermann <refael@empeeric.com>
2013-12-21 11:58:37 +0200

    No more timepicker

--- example/classic/models/embed.js
+++ example/classic/models/embed.js
@@ -1,29 +1,47 @@
 //noinspection JSUnresolvedVariable
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    SchemaTypes = Schema.Types;
+  Schema = mongoose.Schema,
+  SchemaTypes = Schema.Types;
 
 
 var schema = new mongoose.Schema({
-    embeded: {
-        name1: String,
-        list1: [{
-            name2: String,
-            embeded2: {
-                name3: String,
-                list3:[{
-                    name4: String,
-                    embeded4: {
-                        nested_string5: { type: String },
-                        nested_string_req5: { type: String, required: true },
-                        list5:[Number],
-                        pic: SchemaTypes.Picture,
-                        pics: [SchemaTypes.Picture],
-                        refs: [{ type: SchemaTypes.ObjectId, ref: 'pages' }]
-                    }
-                }]
+  embeded: {
+    name1: String,
+    list1: [
+      {
+        name2: String,
+        embeded2: {
+          name3: String,
+          list3: [
+            {
+              name4: String,
+              embeded4: {
+                nested_string5: { type: String },
+                nested_string_req5: { type: String, required: true },
+                list5: [Number],
+                pic: SchemaTypes.Picture,
+                pics: [SchemaTypes.Picture],
+                refs: [
+                  { type: SchemaTypes.ObjectId, ref: 'pages' }
+                ]
+              }
             }
-        }]
+          ]
+        }
+      }
+    ]
+  },
+  available: [
+    {
+      date: Date,
+      hours: [
+        {
+          start: SchemaTypes.Time,
+          end: SchemaTypes.Time,
+          location: SchemaTypes.GeoPoint
+        }
+      ]
     }
+  ]
 });
 var model = module.exports = mongoose.model('embed', schema);
