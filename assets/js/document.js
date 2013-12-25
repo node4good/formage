57a56e0a0e291d6f834a79735ab7ef622a110527
Refael Ackermann <refael@empeeric.com>
2013-12-25 21:06:36 +0200

    Picker is live event

--- assets/js/document.js
+++ assets/js/document.js
@@ -97,10 +97,6 @@ function initWidgets(ctx) {
         $(this).val(JSON.stringify(file));
         var a = $(this).parent().find('a').text(file.filename).attr('href', file.url);
     });
-
-    $('button.picker').on('click', trigger_filepicker);
-    $('button.editor').on('click', trigger_refilepicker);
-    $('button.clearer').on('click', trigger_clearpicker);
 }
 
 
@@ -343,7 +339,79 @@ function initModal() {
         modal.find('iframe').attr("src", "about:blank");
         delete modal.parentSelector;
     });
+}
 
+
+if (window.Aviary) window.featherEditor = new Aviary.Feather({
+    apiKey: window.AVIARY_API_KEY,
+    apiVersion: 3,
+    theme: 'dark', // Check out our new 'light' and 'dark' themes!
+    tools: 'resize,crop,enhance',
+    appendTo: '',
+    onSave: function (imageID, newURL) {
+        var $image = $(document.getElementById(imageID));
+        var OrigInkBlob = $image[0].InkBlob;
+        window.filepicker.writeUrl(OrigInkBlob, newURL, function (NewInkBlob) {
+            $image.attr('src', NewInkBlob.url + '?' + Date.now());
+            var $input = $image.parent().parent().find('input');
+            $input.val(JSON.stringify(NewInkBlob));
+            $input.parent().find('a.file-link').attr('href', NewInkBlob.url);
+            featherEditor.close();
+        });
+    }
+});
+
+
+function handle_file_picked($field, InkBlob) {
+    var $image = $field.find('img.thumb-picker');
+    $image.attr('src', InkBlob.url);
+    $image[0].InkBlob = InkBlob;
+    $field.find(".picker").text('Pick New File');
+    $field.find(".editor").show();
+    $field.find(".clearer").show();
+    $field.find('input').val(JSON.stringify(InkBlob));
+}
+
+
+function handle_file_picked_aviary($field, InkBlob) {
+    handle_file_picked($field, InkBlob);
+    var $image = $field.find('img.thumb-picker');
+    featherEditor.launch({image: $image[0], url: InkBlob.url});
+}
+
+
+function trigger_picturepicker(e) {
+    e.preventDefault();
+    var $field = $(e.target).parent().parent();
+    window.filepicker.pick({mimetype: 'image/*'}, handle_file_picked.bind(null, $field));
+}
+
+
+function trigger_aviarypicker(e) {
+    e.preventDefault();
+    var $field = $(e.target).parent().parent();
+    window.filepicker.pick({mimetype: 'image/*'}, handle_file_picked_aviary.bind(null, $field));
+}
+
+
+function trigger_refilepicker(e) {
+    e.preventDefault();
+    var $field = $(e.target).parent().parent();
+    var serInkBlob = $field.find('input').val();
+    var InkBlob = JSON.parse(serInkBlob);
+    handle_file_picked($field, InkBlob);
+}
+
+
+function trigger_clearpicker(e) {
+    e.preventDefault();
+    var $field = $(e.target).parent().parent();
+    $field.find('input').val('');
+    $field.find(".picker").text('Pick File');
+    $field.find(".editor").hide();
+    $field.find(".clearer").hide();
+    $field.find('img.thumb-picker').attr('src', 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D');
+    $field.find('a.file-link').attr('href', '#')
 }
 
 
@@ -379,6 +447,11 @@ $(function () {
             }
         });
     });
+
+    $('button.picker').on('click', trigger_picturepicker);
+    $('button.aviary-picker').on('click', trigger_aviarypicker);
+    $('button.editor').on('click', trigger_refilepicker);
+    $('button.clearer').on('click', trigger_clearpicker);
 
     initActions();
 });
