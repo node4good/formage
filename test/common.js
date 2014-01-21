b299fd42f6771367f4af14ac3a2efee83ee9d4a7
Refael Ackermann <refael@empeeric.com>
2014-01-21 18:50:27 +0200

    cleaning and getting tests to lint

--- test/common.js
+++ test/common.js
@@ -1,15 +1,15 @@
 'use strict';
-global._ = require('lodash');
-var chai = require('chai');
-var Promise = require('mpromise');
-global.should = chai.should();
-global.expect = chai.expect;
 require('nodestrum');
 Error.stackTraceLimit = 100;
+
+var _ = require('lodash');
+var chai = require('chai');
+var Promise = require('mpromise');
+var fs = require('fs');
+
 process.env.FORMAGE_DISABLE_DOMAINS = true;
 process.env.MONGOOSE_DISABLE_STABILITY_WARNING = true;
 
-//noinspection JSUnusedLocalSymbols
 global.mock_req_proto = {
     method: "get",
     session: {},
@@ -30,27 +30,27 @@ function magic_throw() {
 }
 
 
-//noinspection JSUnusedGlobalSymbols
-global.mock_res_proto = {
-    setHeader: function () {},
-    status: function (val) {this._status = val;},
+var mock_res_proto = {
+    setHeader: _.identity,
+    status: function (val) {
+        this.setHeader(val);
+        this._status = val;
+    },
     output: {push: _.identity},
     outputEncodings: {push: _.identity},
     render: magic_throw,
     redirect: magic_throw
 };
 
+global._ = _;
+global.should = chai.should();
+global.expect = chai.expect;
+global.mock_res_proto = mock_res_proto;
+global.makeRes = function makeRes(req, done) { return _.defaults({ req: req, send: function (status, err) {done(err);} }, mock_res_proto); };
 
-global.makeRes = function makeRes(req, done) {
-    var res = _.defaults({ req: req }, mock_res_proto);
-    res.send = function (status, err) {done(err);};
-    return res;
-};
-
-
-var fs = require('fs');
 global.test_post_body_multipart = fs.readFileSync('test/fixtures/test-post-body.mime', 'utf-8');
 global.renderedEmbeded = fs.readFileSync('test/fixtures/rendered-embed-form.txt', 'utf-8');
+
 global.mockFind = function mockFindFactory(arr) {
     return function mockFind() {
         return {
