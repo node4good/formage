e7e872e81b04d23ef35afbfba4fdc6c24cec5333
Refael Ackermann <refael@empeeric.com>
2014-04-04 16:32:32 +0300

    Moving to bower

--- assets/vendor/select2/release.sh
+++ assets/vendor/select2/release.sh
@@ -5,7 +5,7 @@ echo -n "Enter the version for this release: "
 
 read ver
 
-if [ ! $ver ]; then 
+if [ ! $ver ]; then
 	echo "Invalid version."
 	exit
 fi
@@ -15,20 +15,39 @@ js="$name.js"
 mini="$name.min.js"
 css="$name.css"
 release="$name-$ver"
-tag="release-$ver"
+tag="$ver"
 branch="build-$ver"
 curbranch=`git branch | grep "*" | sed "s/* //"`
 timestamp=$(date)
 tokens="s/@@ver@@/$ver/g;s/\@@timestamp@@/$timestamp/g"
 remote="github"
 
+echo "Pulling from origin"
+
+git pull
+
+echo "Updating Version Identifiers"
+
+sed -E -e "s/\"version\": \"([0-9\.]+)\",/\"version\": \"$ver\",/g" -i -- bower.json select2.jquery.json component.json composer.json package.json
+
+git add bower.json
+git add select2.jquery.json
+git add component.json
+git add composer.json
+git add package.json
+
+git commit -m "modified version identifiers in descriptors for release $ver"
+git push
+
 git branch "$branch"
 git checkout "$branch"
 
 echo "Tokenizing..."
 
-find . -name "$js" | xargs -I{} sed -e "$tokens" -i "" {} 
-find . -name "$css" | xargs -I{} sed -e "$tokens" -i "" {}
+find . -name "$js" | xargs -I{} sed -e "$tokens" -i -- {}
+find . -name "$css" | xargs -I{} sed -e "$tokens" -i -- {}
+
+sed -e "s/latest/$ver/g" -i -- bower.json
 
 git add "$js"
 git add "$css"
@@ -40,15 +59,12 @@ cat LICENSE | sed "$tokens" >> "$mini"
 echo "*/" >> "$mini"
 
 curl -s \
-	-d compilation_level=SIMPLE_OPTIMIZATIONS \
-	-d output_format=text \
-	-d output_info=compiled_code \
 	--data-urlencode "js_code@$js" \
-	http://closure-compiler.appspot.com/compile \
+	http://marijnhaverbeke.nl/uglifyjs \
 	>> "$mini"
 
 git add "$mini"
-	
+
 git commit -m "release $ver"
 
 echo "Tagging..."
